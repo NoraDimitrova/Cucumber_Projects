34d4c72d52ea83729cc95f4e489e5054ddd2d175
Nora Dimitrova <nora_dd@abv.bg>
2023-01-02 16:05:38 +0200

    modified TestRunner.java

--- src/test/java/StepDefinition/TestRunner.java
+++ src/test/java/StepDefinition/TestRunner.java
@@ -5,7 +5,9 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources/Features", glue={"StepDefinition"})
+@CucumberOptions(features="src/test/resources/Features", glue={"StepDefinition"}, monochrome = true,
+       plugin={"pretty", "json:target/JSONReports/report.json"}
+)
 
 public class TestRunner {
 
